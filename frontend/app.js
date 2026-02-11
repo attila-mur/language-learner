b12fda971c69fda490179e464292adfaf37bcfe5
Attila <attila.murvai.dev@gmail.com>
2026-02-11 02:56:07 +0100

    Fix host

--- frontend/app.js
+++ frontend/app.js
@@ -1,6 +1,6 @@
 // Configuration
 // const API_BASE = 'http://localhost:8080/api';
-const API_BASE = 'https://https://language-learner-d2c8.onrender.com/'
+const API_BASE = 'https://language-learner-d2c8.onrender.com/'
 
 // State
 let currentCards = [];
